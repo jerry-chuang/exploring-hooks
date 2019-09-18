f0362f3c74689eb6fb76b1aef1e9cf1a1740a7c0
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-09-17 17:15:41 -0700

    Finish tutorial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import UserTable from './tables/UserTable';
 import AddUserForm from './forms/AddUserForm'
+import EditUserForm from './forms/EditUserForm'
 
 const App = () => {
   const usersData = [
@@ -9,7 +10,17 @@ const App = () => {
     { id: 3, name: 'Ben', username: 'benisphere' },
   ];
 
+
   const [users, setUsers] = useState(usersData);
+  const [editing, setEditing] = useState(false)
+  const initialFormState = { id: null, name: '', username: '' }
+  const [currentUser, setCurrentUser] = useState(initialFormState)
+
+  const editRow = user => {
+    setEditing(true)
+  
+    setCurrentUser({ id: user.id, name: user.name, username: user.username })
+  }
 
   const addUser = user => {
     user.id = users.length +1
@@ -20,17 +31,38 @@ const App = () => {
     setUsers(users.filter(user => user.id !== id))
   }
 
+  const updateUser = (id, updatedUser) => {
+    setEditing(false)
+  
+    setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+  }
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
-        <div className="flex-large">
-          <h2>Add user</h2>
-          <AddUserForm addUser={addUser} />
-        </div>
+      <div className="flex-large">
+        {editing ? (
+          <div>
+            <h2>Edit user</h2>
+            <EditUserForm
+              editing={editing}
+              setEditing={setEditing}
+              currentUser={currentUser}
+              updateUser={updateUser}
+            />
+          </div>
+        ) : (
+          <div>
+            <h2>Add user</h2>
+            <AddUserForm addUser={addUser} />
+          </div>
+        )}
+      </div>
+
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable users={users} deleteUser={deleteUser} />
+          <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
         </div>
       </div>
     </div>
